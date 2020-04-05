a25ba6a2ec1ee039930f645ad4eb41e7da3af7e5
shofwanh <shofwanhanif22@gmail.com>
2020-04-05 13:45:44 +0700

    Feature Search

---
+++ src/components/home/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="box">
+            <h1 class="title">Search Your Domain</h1>
+            <div class="search">
+                <input 
+                    type="text" 
+                    placeholder="namadomain.com" 
+                    v-model="keyword" 
+                    class="search__input"                    
+                />
+                <button class="search__button">
+                    <span class="icon-search"></span>
+                    Search
+                </button>
+            </div>
+
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data(){
+        return {
+            keyword: '',
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
