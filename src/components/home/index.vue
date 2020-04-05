b8dcc7248f9a88fa3b0d0f39c1b818ed737d7c56
shofwanh <shofwanhanif22@gmail.com>
2020-04-05 15:53:08 +0700

    Feature Search Available Domain

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -7,15 +7,25 @@
                     type="text" 
                     placeholder="namadomain.com" 
                     v-model="keyword" 
-                    class="search__input"                    
+                    class="search__input"
+                    :class="{'error':isEmpty}"
+                    @input="onInput" 
+                    @keyup.enter="onSearch"                    
                 />
-                <button class="search__button">
+                <button class="search__button" @click="onSearch">
                     <span class="icon-search"></span>
                     Search
                 </button>
             </div>
 
-            
+            <div v-if="results.length > 0" class="result">
+                <div class="result__list" v-for="(result,index) in results" :key="index">
+                    <p class="result__list__name">Domain is available!</p>
+                    <button v-if="!result.status" class="result__list__buy" @click="onClickBuy(result)">Buy</button>
+                    <span v-else class="result__list__purchased">Purchased</span>
+                </div>
+            </div>
+                     
         </div>
     </div>
 </template>
@@ -26,8 +36,46 @@ export default {
     data(){
         return {
             keyword: '',
-            
+            results: [],
+            recommendations: [],
+            isSubmit: false,
+            domainList: [
+                {
+                    name: 'domainyanglagidicari.space',
+                    price: 'Rp 100.000',
+                    status: false
+                },
+                {
+                    name: 'domainyanglagidicari.net',
+                    price: 'Rp 50.000',
+                    status: false
+                },
+                {
+                    name: 'domainyanglagidicari.org',
+                    price: 'Rp 70.000',
+                    status: false
+                },
+                {
+                    name: 'domainlain.com',
+                    price: 'Rp 30.000',
+                    status: false
+                },
+                {
+                    name: 'domainlain.net',
+                    price: 'Rp 10.000',
+                    status: false
+                }
+            ]            
         }
+    },    
+    methods: {
+        onSearch(){
+            this.isSubmit = true
+            this.results = this.domainList.filter(domainList => (domainList.name.toLowerCase()) == this.keyword.toLowerCase())
+        },        
+        onInput(){
+            this.isSubmit = false
+        },        
     }
 }
 </script>
